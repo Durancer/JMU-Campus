c60f7535e4063ca1a33a1c1789053b0f775188f9
durancer <1160888352@qq.com>
2023-01-26 22:06:47 +0800

    网关基础框架搭建

---
+++ gateway-service/src/main/java/com/xueyu/gateway/GatewayApplication.java
@@ -0,0 +1,18 @@
+package com.xueyu.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 网关服务
+ *
+ * @author durance
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class);
+	}
+
+}
