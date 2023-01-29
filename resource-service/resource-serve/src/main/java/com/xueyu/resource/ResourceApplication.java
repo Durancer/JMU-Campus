cd34057c539cece06082f51cdeca9fe11b32f36b
durancer <1160888352@qq.com>
2023-01-29 15:14:18 +0800

    搭建资源服务架构

---
+++ resource-service/resource-serve/src/main/java/com/xueyu/resource/ResourceApplication.java
@@ -0,0 +1,16 @@
+package com.xueyu.resource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author durance
+ */
+@SpringBootApplication
+public class ResourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceApplication.class);
+	}
+
+}
