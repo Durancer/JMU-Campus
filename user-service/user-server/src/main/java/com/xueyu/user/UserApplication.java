3498ab17ddc5ef9a04f5f369116448518d54c1ea
durancer <1160888352@qq.com>
2023-01-30 16:38:06 +0800

    完善UserApplication启动类

--- user-service/user-server/src/main/java/com/xueyu/user/UserApplication.java
+++ user-service/user-server/src/main/java/com/xueyu/user/UserApplication.java
@@ -1,6 +1,7 @@
 package com.xueyu.user;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
@@ -9,5 +10,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @MapperScan("com.xueyu.user.mapper")
 public class UserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserApplication.class);
+	}
 
 }
