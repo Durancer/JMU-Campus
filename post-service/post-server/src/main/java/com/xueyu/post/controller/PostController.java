9794d728fc5fe45f075998b99db13ec8a0a90ed2
durancer <1160888352@qq.com>
2023-02-05 22:38:52 +0800

    完善查询post列表接口，添加接口层代码

--- post-service/post-server/src/main/java/com/xueyu/post/controller/PostController.java
+++ post-service/post-server/src/main/java/com/xueyu/post/controller/PostController.java
@@ -1,11 +1,12 @@
 package com.xueyu.post.controller;
 
+import com.xueyu.common.core.result.ListVO;
 import com.xueyu.common.core.result.RestResult;
 import com.xueyu.post.pojo.domain.Post;
+import com.xueyu.post.pojo.vo.PostListVO;
 import com.xueyu.post.service.PostService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.lang.NonNull;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
@@ -29,13 +30,54 @@ public class PostController {
 	 * @param files 图片文件
 	 * @return 发布结果
 	 */
-	@PostMapping("addPost")
+	@PostMapping("add")
 	public RestResult<?> pushlishPost(Post post, MultipartFile[] files) {
 		Boolean sendStatus = postService.publishPost(post, files);
 		if (!sendStatus) {
 			return RestResult.fail("发布失败");
 		}
-		return RestResult.ok("发布成功");
+		return RestResult.ok(null, "发布成功");
+	}
+
+	/**
+	 * 删除用户帖子
+	 *
+	 * @param postId 帖子id
+	 * @param userId 用户id
+	 * @return 删除结果
+	 */
+	@PostMapping("delete")
+	public RestResult<?> deletePost(@NonNull Integer postId, @RequestHeader Integer userId) {
+		if (!postService.deletePost(postId, userId)) {
+			return RestResult.fail("删除失败");
+		}
+		return RestResult.ok(null, "删除成功");
+	}
+
+	/**
+	 * 分页获取所有帖子
+	 *
+	 * @param current 当前页
+	 * @param size    每页数量
+	 * @return 帖子信息
+	 */
+	@GetMapping("list/all")
+	public RestResult<ListVO<PostListVO>> getAllPost(Integer current, Integer size) {
+		ListVO<PostListVO> postListByPage = postService.getPostListByPage(current, size, null);
+		return RestResult.ok(postListByPage);
+	}
+
+	/**
+	 * 分页用户帖子
+	 *
+	 * @param current 当前页
+	 * @param size    每页数量
+	 * @return 帖子信息
+	 */
+	@GetMapping("list/all")
+	public RestResult<ListVO<PostListVO>> getAllPost(Integer current, Integer size, @RequestHeader Integer userId) {
+		ListVO<PostListVO> postListByPage = postService.getPostListByPage(current, size, userId);
+		return RestResult.ok(postListByPage);
 	}
 
 }
