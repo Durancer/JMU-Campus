c0a1e5eee85a67284690d74b865e5ae06b3461ce
durancer <1160888352@qq.com>
2023-04-16 21:50:28 +0800

    增加全系统单 ip限制规则

---
+++ gateway-service/src/main/java/com/xueyu/gateway/filter/AccessLimitFilter.java
@@ -0,0 +1,71 @@
+package com.xueyu.gateway.filter;
+
+import com.xueyu.gateway.util.RequestUtil;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 限制单ip访问，防止单ip频繁访问
+ *
+ * @author durance
+ */
+@Component
+public class AccessLimitFilter implements GlobalFilter, Ordered {
+
+	@Resource
+	RedisTemplate<String, Integer> redisTemplate;
+
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+		ServerHttpRequest request = exchange.getRequest();
+		// 获取请求ip地址
+		String ipAddress = RequestUtil.getIpAddress(request);
+		if (checkLimit(ipAddress)) {
+			// 如果超限了，则返回频繁访问信息
+			ServerHttpResponse response = exchange.getResponse();
+			response.setStatusCode(HttpStatus.REQUEST_TIMEOUT);
+			return response.setComplete();
+		}
+		return chain.filter(exchange);
+	}
+
+	/**
+	 * 核对ip地址是否超出访问限制
+	 * 当前限流策略为 10s最多访问 30次
+	 *
+	 * @param ipAddress ip地址
+	 * @return 是否超限
+	 */
+	public boolean checkLimit(String ipAddress) {
+		// todo 将限流参数转移到配置文件当中
+		int time = 10, count = 30;
+		String key = "limit:" + ipAddress;
+		Integer hasLimit = redisTemplate.opsForValue().get(key);
+		if (hasLimit != null) {
+			if (hasLimit == 0) {
+				return true;
+			}
+			redisTemplate.opsForValue().decrement(key);
+		} else {
+			redisTemplate.opsForValue().set(key, count, time, TimeUnit.SECONDS);
+		}
+		return false;
+	}
+
+	@Override
+	public int getOrder() {
+		return -2;
+	}
+
+}
