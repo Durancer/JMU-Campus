dd10885267238b01f1e6452e1d9de3420bbcb218
王雨 <1547025615@qq.com>
2023-08-03 23:40:35 +0800

    feat:增加发布页面&&调整项目路由&&增加本地token

---
+++ campus-web/src/utils/common.ts
@@ -0,0 +1,24 @@
+import { localCache } from './cache'
+import { ElMessage } from 'element-plus'
+// import { getCurrentInstance } from 'vue'
+// 判断是否登录
+export function isLogin() {
+  return localCache.getCache('token')
+}
+
+// 成功的消息提示
+export function sucMessage(message: string) {
+  ElMessage({
+    message,
+    type: 'success'
+  })
+}
+
+// 失败的消息提示
+// export function failMessage(message: string) {
+//   // const { appContext } = getCurrentInstance()!
+//   return ElMessage({
+//     message,
+//     type: 'error'
+//   })
+// }
