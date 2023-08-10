c1081ffa02a3ff2ba567b0c129052fe5cf9c8226
durancer <1160888352@qq.com>
2023-08-10 16:03:46 +0800

    将错误描述展示到接口

--- user-service/user-server/src/main/java/com/xueyu/user/exception/UserException.java
+++ user-service/user-server/src/main/java/com/xueyu/user/exception/UserException.java
@@ -1,11 +1,13 @@
 package com.xueyu.user.exception;
 
+import com.xueyu.common.core.exception.BusinessException;
+
 /**
  * 用户服务异常
  *
  * @author durance
  */
-public class UserException extends RuntimeException {
+public class UserException extends BusinessException {
 
 	public UserException() {
 		super("用户服务异常");
