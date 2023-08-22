69f474e633fd924f6b426602e95bee3bf72df48b
王雨 <1547025615@qq.com>
2023-08-22 23:14:26 +0800

    feat: 发帖增加话题功能

--- campus-web/src/views/publish/index.vue
+++ campus-web/src/views/publish/index.vue
@@ -1,19 +1,6 @@
 <template>
   <div class="editor">
     <el-input placeholder="请输入帖子标题" class="edit-input" v-model="post.title" />
-    <!--
-    <div
-      class="content"
-      contenteditable="true"
-      @input="handleInput"
-      @blur="handleInput"
-      ref="contentRef"
-    >
-      <div class="placeholder" style="pointer-events: none" :class="{ hasContent: hasContent }">
-        此刻你想和大家分享什么
-      </div>
-    </div>
-    -->
     <div style="border: 1px solid #ccc">
       <Toolbar
         style="border-bottom: 1px solid #ccc"
@@ -30,13 +17,40 @@
       />
     </div>
   </div>
-  <div class="wrapper" v-show="isShow">
-    <Vote ref="voteRef"></Vote>
-  </div>
+
   <div class="wrapper">
     <div>
+      是否增加话题
+      <el-switch v-model="isAddTopic"></el-switch>
+    </div>
+    <div v-show="isAddTopic">
+      <div>
+        <span>请输入增加的话题(最多三个)</span>
+        <el-select
+          v-model="topic"
+          multiple
+          filterable
+          allow-create
+          default-first-option
+          :reserve-keyword="false"
+          placeholder="请输入话题"
+          size="large"
+        >
+          <el-option
+            v-for="item in options"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          />
+        </el-select>
+      </div>
+    </div>
+    <div>
       是否增加投票
-      <el-switch v-model="isShow" />
+      <el-switch v-model="isAddVote" />
+    </div>
+    <div class="wrapper" v-show="isAddVote">
+      <Vote ref="voteRef"></Vote>
     </div>
     <el-button type="success" @click="publish">发布</el-button>
   </div>
@@ -49,7 +63,6 @@ import { sucMessage, failMessage } from '@/utils/common.ts'
 import { useRouter } from 'vue-router'
 import { ElMessage } from 'element-plus'
 import Vote from '@/components/Vote.vue'
-// import '@wangeditor/editor/dist/css/style.css' // 引入 css
 import { Editor, Toolbar } from '@wangeditor/editor-for-vue'
 
 const contentRef = ref(null)
@@ -57,8 +70,25 @@ const post = reactive({
   title: '',
   content: ''
 })
-const isShow = ref(false)
+const isAddTopic = ref(false)
+const isAddVote = ref(false)
 const voteRef = ref(null)
+const topic = ref<string[]>([])
+// 临时的数据
+const options = [
+  {
+    value: 'HTML',
+    label: 'HTML'
+  },
+  {
+    value: 'CSS',
+    label: 'CSS'
+  },
+  {
+    value: 'JavaScript',
+    label: 'JavaScript'
+  }
+]
 // 编辑器实例，必须用 shallowRef
 const editorRef = shallowRef()
 const mode = 'defalut'
@@ -87,13 +117,10 @@ onBeforeUnmount(() => {
 const handleCreated = (editor) => {
   editorRef.value = editor // 记录 editor 实例，重要！
 }
-const handleInput = (event) => {
-  post.content = event.target.innerText
-}
-// const titleInput = (e) => {
-//   console.log(e.target.value, post.title)
+// const handleInput = (event) => {
+//   post.content = event.target.innerText
 // }
-const hasContent = computed(() => post.content.length > 0)
+
 const valid = () => {
   if (!post.title) {
     failMessage('请输入标题')
@@ -107,15 +134,16 @@ const valid = () => {
 }
 const router = useRouter()
 const publish = () => {
-  // post.content = contentRef.value?.innerText
   if (valid()) {
-    let data = {}
-    if (isShow.value) {
+    let data = { ...post }
+    if (isAddVote.value) {
       // 增加投票选项
       const voteContent = JSON.parse(JSON.stringify(voteRef.value.voteForm)) //TODO 之后得优化
       data = { ...voteContent, ...post }
-    } else {
-      data = { ...post }
+    }
+    // 如果增加话题选项
+    if (isAddTopic.value) {
+      data = { ...data, names: topic.value }
     }
     let formData = new FormData()
     for (let key in data) {
@@ -143,16 +171,9 @@ const totast = () => {
 @import url('@wangeditor/editor/dist/css/style.css');
 .editor {
   box-sizing: border-box;
-  width: 930px;
-  padding: 20px;
-  margin: auto;
-  border-radius: 12px;
-  background-color: rgba(255, 255, 255, 1);
-  overflow: auto;
+  max-width: 930px;
+  margin: 20px auto;
   .edit-input {
-    // background-color: transparent;
-    // border: none;
-    // border-bottom: 1px solid rgba(240, 240, 240, 1);
     color: rgba(51, 51, 51, 1);
     font-size: 16px;
     font-weight: 300;
@@ -163,31 +184,12 @@ const totast = () => {
     width: 100%;
     margin-bottom: 20px;
   }
-  .content {
-    margin-top: 10px;
-    position: relative;
-    min-height: 240px;
-    .placeholder {
-      position: absolute;
-      left: 0;
-      right: 0;
-      color: rgba(51, 51, 51, 1);
-      font-size: 14px;
-      font-weight: 300;
-    }
-    &:focus {
-      outline: none;
-    }
-    &:focus .placeholder {
-      display: none;
-    }
-    .hasContent {
-      display: none;
-    }
-  }
 }
 .wrapper {
-  width: 930px;
+  max-width: 930px;
   margin: 20px auto;
+}
+.el-select {
+  width: 320px;
 }
 </style>
